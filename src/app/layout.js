6a53760090f667c85358beddcf8f9170c7051be3
krushnasaruk55 <krushnasaruk1234@gmail.com>
2026-06-06 11:57:06 +0530

    refactor: update main layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
       <head>
         {/* Anti-Flash Theme Injector */}
         <script dangerouslySetInnerHTML={{
