84a8e1493386ebe5fe72ade805425bc60be83bc9
krushnasaruk55 <krushnasaruk1234@gmail.com>
2026-06-12 01:11:39 +0530

    Incremental update part 1 of 17

--- _deploy/next-app/.next/server/app/api/assistant/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/assistant/route.js")
-R.c("server/chunks/[root-of-the-server]__0izpa2c._.js")
-R.c("server/chunks/[root-of-the-server]__0j8-xkl._.js")
-R.c("server/chunks/node_modules_@google_generative-ai_dist_index_mjs_07lt0._._.js")
-R.c("server/chunks/_next-internal_server_app_api_assistant_route_actions_0.znwuh.js")
-R.m(74638)
-module.exports=R.m(74638).exports
--- _deploy/next-app/.next/server/app/api/downloads/[filename]/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/downloads/[filename]/route.js")
-R.c("server/chunks/[root-of-the-server]__0sp8vja._.js")
-R.c("server/chunks/[root-of-the-server]__0j8-xkl._.js")
-R.c("server/chunks/_next-internal_server_app_api_downloads_[filename]_route_actions_11af.m-.js")
-R.m(72024)
-module.exports=R.m(72024).exports
